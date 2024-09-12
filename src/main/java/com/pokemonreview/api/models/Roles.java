eb37f1e54ceceddf880a35352eddb740dd86d026
Pravesh Ponnuvelu <pponnuve@sundevils.asu.edu>
2024-09-11 20:54:12 -0400

    first-commit

---
+++ src/main/java/com/pokemonreview/api/models/Roles.java
@@ -0,0 +1,19 @@
+package com.pokemonreview.api.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+
+
+@Setter
+@Getter
+@Entity
+@Table(name = "roles")
+public class Roles {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+}
---
+++ src/main/java/com/pokemonreview/api/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.pokemonreview.api.repository;
+
+import com.pokemonreview.api.models.Roles;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Roles, Integer> {
+    Optional<Roles> findByName(String name);
+}
